12f357340e2470cc485d43f3047b76f11ae88b33
Drakaniia <floresaybaez574@gmail.com>
2026-04-03 09:53:24 +0800

    feat(backend): add messaging system backend structure with MVC architecture

---
+++ server/src/Modules/Message/Controllers/MessageController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Modules\Message\Controllers;
+
+use Modules\Message\Services\MessageService;
+
+/**
+ * Message Controller
+ * Handles HTTP requests for messaging system
+ */
+class MessageController
+{
+    // TODO: Add MessageService property
+    
+    // TODO: Add constructor to initialize service
+    
+    // TODO: Add method to get user's conversations
+    // public function index($request) { ... }
+    
+    // TODO: Add method to get specific conversation with messages
+    // public function show($request, $conversationId) { ... }
+    
+    // TODO: Add method to create/send a new message
+    // public function store($request) { ... }
+    
+    // TODO: Add method to mark messages as read
+    // public function markAsRead($request, $conversationId) { ... }
+    
+    // TODO: Add method to search messages
+    // public function search($request) { ... }
+    
+    // TODO: Add method to download attachment
+    // public function downloadAttachment($request, $attachmentId) { ... }
+    
+    // TODO: Add method to delete a message
+    // public function destroy($request, $messageId) { ... }
+}
